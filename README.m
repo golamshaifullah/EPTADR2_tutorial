7f55762a8f1a44cecacf4c13e78065e89e4242d9
Golam Shaifullah <golam.shaifullah@unimib.it>
2024-09-16 23:29:51 +0200

    Fresh commit

---
+++ README.m
@@ -0,0 +1,16 @@
+# A set of tutorials to use with the EPTA DR2 data release
+
+You'll need colab to run these tutorials. You can run them on your local machine, but you'll need to install the required packages.
+
+## Tutorial 1: Introduction to pulsar timing with libstempo
+
+This tutorial will introduce you to the basics of pulsar timing analysis using the libstempo package. We will use the EPTA DR2 data release to perform a simple timing analysis of a pulsar.
+
+## Tutorial 2: Searching for red noise and dispersion measure variations
+
+This tutorial will show you how to search for red noise and dispersion measure variations in pulsar timing data. We will use the EPTA DR2 data release to perform a simple analysis of a pulsar.
+
+## Tutorial 3: Fitting a gravitational wave signal
+
+  In thise tutorial we will fit a gravitational wave signal to the EPTA DR2 data release. We will use the libstempo package to perform a simple analysis of a pulsar.
+  
